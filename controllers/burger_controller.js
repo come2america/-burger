9feb74640739226663f9b242408a6a703b4058a1
Yusuf <ysserunjogi@gmail.com>
2019-02-24 23:50:29 -0800

    tried my best

---
+++ controllers/burger_controller.js
@@ -0,0 +1,63 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burgermod = require("../models/burger.js");
+
+router.get("/", function(req, res){
+    res.redirect("/burgers");
+})
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/burgers", function(req, res) {
+  burgermod.all(function(data) {
+  console.log(data)
+    res.render("index", { Burgers: data});
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burgermod.create([
+    "Burgers", "Devoured"
+  ], [
+    req.body.Burgers, req.body.Devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+    res.render("index", { Burgers: data});
+  });
+});
+
+// router.put("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.ID;
+
+//   console.log("condition", condition);
+
+//   burger.update({
+//     Devoured: req.body.Devoured
+//   }, condition, function(result) {
+//     if (result.changedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
+router.delete("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  cat.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
--- server.js
+++ server.js
@@ -11,13 +11,19 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burger_controller.js");
 
-var connection =require("./config/connection")
+app.use(routes);
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
