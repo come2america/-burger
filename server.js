882e696eccc2d5d811e52dd40b61db81e7455041
Yusuf <ysserunjogi@gmail.com>
2019-02-25 00:01:41 -0800

    WORK PLEASE

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
